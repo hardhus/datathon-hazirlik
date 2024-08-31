c29224859c8a0372c3c36f1df4b6837dd763a23f
hardhus <ahmetcemiltoprakk@gmail.com>
2024-08-31 10:29:20 +0300

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/settings.json
    	new file:   datathon2023/data/sample_submission.csv
    	new file:   datathon2023/data/test_x.csv
    	new file:   datathon2023/data/train.csv
    	new file:   datathon2023/datathon2023.zip
    	new file:   main.py
    	new file:   requirements.txt
    	new file:   tensorflow/main.py

---
+++ tensorflow/main.py
@@ -0,0 +1,11 @@
+# 
+# Author: hardhus (ahmetcemiltoprakk@gmail.com)
+# 
+# File: tensorflow\main.py (c) 2024
+# Project: DATATHON
+# 
+# Created:  2024-08-31T07:18:47.253Z
+# Modified: <date>
+# 
+
+print("ŞEYMA NEREDE?")
